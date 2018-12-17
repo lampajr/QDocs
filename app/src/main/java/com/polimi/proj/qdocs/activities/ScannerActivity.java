e67d8c553dd78c41af989fcd29ab4a013c024d96
lampajr <lampa9559@gmail.com>
2018-12-17 12:36:21 +0100

    ScannerActivity initial implementation

--- app/src/main/java/com/polimi/proj/qdocs/activities/ScannerActivity.java
+++ app/src/main/java/com/polimi/proj/qdocs/activities/ScannerActivity.java
@@ -1,15 +1,214 @@
 package com.polimi.proj.qdocs.activities;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.ImageReader;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.TextureView;
+import android.widget.Button;
 
 import com.polimi.proj.qdocs.R;
 
 public class ScannerActivity extends AppCompatActivity {
 
+    private static final String TAG = "SCANNER";
+    private static final int REQUEST_CAMERA_PERMISSION = 2;
+
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
+
+    private Button takePictureButton;
+    private TextureView textureView;
+
+    private CameraDevice cameraDevice;
+    private CameraCaptureSession cameraCaptureSession;
+    private CaptureRequest captureRequest;
+    private CaptureRequest.Builder captureRequestBuilder;
+
+    private Size imageDimension;
+    private ImageReader imageReader;
+
+    private Handler mBackgroundHandler;
+    private HandlerThread mBackgroundThread;
+
+    // coordinates of the most recent touch on the screen
+    private double previousX=0.0, previousY=0.0;
+
+    // listener on the Texture View
+    private final TextureView.SurfaceTextureListener textureListener =
+            new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            // called whenever the surface of the textureView is available
+            // open the camera
+            openCamera();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+            // to implement
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+            //to implement
+        }
+    };
+
+    // callback on the state of the Camera Device
+    private final CameraDevice.StateCallback stateCallback =
+            new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            // the camera has been successfully opened
+            Log.d(TAG, "Camera " + camera.getId() + " opened!");
+            cameraDevice = camera;
+            // create the camera preview on the textureView
+            createCameraPreview();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            Log.d(TAG, "Camera " + camera.getId() + " disconnected");
+            camera.close();
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            Log.e(TAG, "the following error occurred " + error);
+            cameraDevice = null;
+        }
+    };
+
+    // callback on the capture session
+    private final CameraCaptureSession.CaptureCallback captureCallback =
+            new CameraCaptureSession.CaptureCallback() {
+        @Override
+        public void onCaptureCompleted(@NonNull CameraCaptureSession session,
+                                       @NonNull CaptureRequest request,
+                                       @NonNull TotalCaptureResult result) {
+            super.onCaptureCompleted(session, request, result);
+
+            //++++++++TO IMPLEMENT++++++++++//
+            // this method is called when the capture of the image goes well
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+
+
+    }
+
+    /**
+     * set a swipe listener on the texture view such that whenever
+     * the user swipe from left to right the screen the login activity
+     * will appear
+     */
+    private void setSwipeListener() {
+
+    }
+
+    /**
+     * start the LoginActivity
+     */
+    private void startLoginActivity() {
+
+    }
+
+    /**
+     * setup the takePictureButton adding the onClickListener
+     */
+    private void setupTakePictureButton() {
+
+    }
+
+    /**
+     * start the background thread and create the handler
+     * from the background thread Looper
+     */
+    private void startBackgroundThread() {
+        mBackgroundThread = new HandlerThread("CAMERA BACKGROUND");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+    }
+
+    /**
+     * stop the background thread
+     */
+    private void stopBackgroundThread() {
+        mBackgroundThread.quitSafely();
+        try {
+            mBackgroundThread.join();
+            mBackgroundThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
+            Log.e(TAG, "Error occurred stopping the background thread: " + e.getMessage());
+        }
+    }
+
+    /**
+     * open the main camera of the current device
+     */
+    private void openCamera() {
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+            String cameraId = manager.getCameraIdList()[0];
+
+            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+
+            assert map != null;
+            imageDimension = map.getOutputSizes(SurfaceTexture.class)[0];
+
+            // check the permission
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                return;
+            }
+            manager.openCamera(cameraId, stateCallback, null);
+        }
+        catch (CameraAccessException e) {
+            Log.e(TAG, "Error occurred accessing the Camera: "+ e.getMessage());
+        }
+    }
+
+    /**
+     * create the camera preview on the texture view
+     */
+    private void createCameraPreview() {
+
     }
 }
