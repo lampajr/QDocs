5ddd456e39ab19528d4dfd0ee31f29791c65fc37
lampajr <lampa9559@gmail.com>
2018-12-19 17:04:33 +0100

    FileViewer added, but empty

---
+++ app/src/main/java/com/polimi/proj/qdocs/activities/FileViewer.java
@@ -0,0 +1,24 @@
+package com.polimi.proj.qdocs.activities;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.polimi.proj.qdocs.R;
+
+public class FileViewer extends AppCompatActivity {
+
+    private DatabaseReference dbRef;
+    private Bundle bundle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_viewer);
+
+        bundle = getIntent().getExtras();
+    }
+
+
+}
