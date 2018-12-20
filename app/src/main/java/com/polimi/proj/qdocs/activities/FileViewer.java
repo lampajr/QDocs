94af2b1427913f7c719c0e79a70f1ac817295859
lampajr <lampa9559@gmail.com>
2018-12-20 17:45:49 +0100

    Show activities created

--- app/src/main/java/com/polimi/proj/qdocs/activities/FileViewer.java
+++ app/src/main/java/com/polimi/proj/qdocs/activities/FileViewer.java
@@ -1,11 +1,18 @@
 package com.polimi.proj.qdocs.activities;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.MimeTypeMap;
 
 import com.google.android.gms.tasks.OnCanceledListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -16,8 +23,12 @@ import com.polimi.proj.qdocs.R;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class FileViewer extends AppCompatActivity {
+
+    // TODO: convert to SERVICE!!!! and in according to the file start the appropriate activity
+    // TODO: copy into RetrieveFile
 
     private static final String TAG = "FILE_VIEWER";
 
@@ -29,6 +40,7 @@ public class FileViewer extends AppCompatActivity {
     private String extension;
     private StorageReference storageRef;
     private File localFile;
+    private static final int REQUEST_SHOW_FILE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,13 +58,17 @@ public class FileViewer extends AppCompatActivity {
         try {
             storageRef = FirebaseStorage.getInstance().getReference().child(user.getUid())
                     .child(wholeFilename);
-            localFile = File.createTempFile(filename, extension);
+            Log.d(TAG, "Cache dir: " + getCacheDir());
+            localFile = File.createTempFile(filename, extension, getCacheDir());
+
+            if (localFile != null) Log.d(TAG, "Local file created");
 
             // download file
             storageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
                     Log.d(TAG, "File downloaded successfully");
+                    showFile();
                 }
             }).addOnCanceledListener(new OnCanceledListener() {
                 @Override
@@ -65,6 +81,27 @@ public class FileViewer extends AppCompatActivity {
             e.printStackTrace();
         }
 
+    }
+
+    /**
+     * show the file downloaded in the temporary location
+     */
+    private void showFile() {
+        Intent showIntent = new Intent(Intent.ACTION_VIEW);
+
+        Uri fileUri = FileProvider.getUriForFile(getApplicationContext(),
+                "com.polimi.proj.qdocs.fileprovider",
+                localFile);
+
+        // grant the permission
+        grantUriPermission("com.polimi.proj.qdocs", fileUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        // get the extension type
+        String mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+        Log.d(TAG, "Showing file: " + fileUri + " -- " + "." + mimeType);
+        //showIntent.setAction(Intent.ACTION_VIEW);
+        showIntent.setDataAndType(fileUri, mimeType);
+        startActivityForResult(showIntent, REQUEST_SHOW_FILE);
     }
 
     @Override
---
+++ app/src/main/java/com/polimi/proj/qdocs/activities/PlayAudioActivity.java
@@ -0,0 +1,15 @@
+package com.polimi.proj.qdocs.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.polimi.proj.qdocs.R;
+
+public class PlayAudioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_audio);
+    }
+}
