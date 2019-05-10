131460e9475c8b52bd5abc96935c289475afcdf7
pietro <pietrochitto@hotmail.it>
2019-05-10 10:07:56 +0200

    color of directory changed color of "add button" changed

---
+++ app/src/test/java/com/polimi/proj/qdocs/MyfileUnitTest.java
@@ -0,0 +1,32 @@
+/*package com.polimi.proj.qdocs;
+import com.polimi.proj.qdocs.support.MyFile;
+import com.polimi.proj.qdocs.support.StorageElement;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MyfileUnitTest {
+
+    private MyFile f1;
+    private MyFile f2;
+    private MyFile f3;
+    private MyFile f4;
+    private List<StorageElement> elements;
+
+    @Before
+    public void setup(){
+        f1 = new MyFile("file1", "pdf", "1", "48", "12",
+                new Long(1234556798), false);
+
+        f2 = new MyFile("file2", "pdf", "2", "64", "12",
+                new Long(123456798), true);
+    }
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}*/
