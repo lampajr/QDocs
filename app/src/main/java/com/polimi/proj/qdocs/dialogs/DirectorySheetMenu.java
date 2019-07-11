b88d35399ba4f9f65fc7c067d563e322aca93b7f
lampajr <lampa9559@gmail.com>
2019-07-11 18:01:44 +0200

    Added dialogs license

--- app/src/main/java/com/polimi/proj/qdocs/dialogs/DirectorySheetMenu.java
+++ app/src/main/java/com/polimi/proj/qdocs/dialogs/DirectorySheetMenu.java
@@ -12,6 +12,22 @@ import androidx.annotation.Nullable;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.polimi.proj.qdocs.R;
 
+/**
+ * Copyright 2018-2019 Lamparelli Andrea & Chittò Pietro
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 public class DirectorySheetMenu extends BottomSheetDialogFragment {
     private View.OnClickListener onDelete, onInfo;
 
--- app/src/main/java/com/polimi/proj/qdocs/dialogs/QrCodeDialog.java
+++ app/src/main/java/com/polimi/proj/qdocs/dialogs/QrCodeDialog.java
@@ -29,6 +29,22 @@ import static com.polimi.proj.qdocs.dialogs.QrCodeDialog.SaveQRCodeTask.ERROR;
 import static com.polimi.proj.qdocs.dialogs.QrCodeDialog.SaveQRCodeTask.FILE_ALREADY_EXIST;
 import static com.polimi.proj.qdocs.dialogs.QrCodeDialog.SaveQRCodeTask.FILE_CREATED;
 
+/**
+ * Copyright 2018-2019 Lamparelli Andrea & Chittò Pietro
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 public class QrCodeDialog extends Dialog {
     private final static String TAG = "QR_CODE_DIALOG";
 
