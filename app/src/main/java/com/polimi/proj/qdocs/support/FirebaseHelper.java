e96d1183228cf7ae514fab54d5acfa4ac18fcd4d
lampajr <lampa9559@gmail.com>
2019-07-11 18:13:02 +0200

    Added support classes license

--- app/src/main/java/com/polimi/proj/qdocs/support/FirebaseHelper.java
+++ app/src/main/java/com/polimi/proj/qdocs/support/FirebaseHelper.java
@@ -18,8 +18,29 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.polimi.proj.qdocs.model.MyFile;
+import com.polimi.proj.qdocs.model.StorageElement;
 
 import java.util.Calendar;
+
+/**
+ * Copyright 2018-2019 Lamparelli Andrea & Chittò Pietro
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Helper class that handles all the interaction between the application and the Firebase database
+ * and storage.
+ */
 
 public class FirebaseHelper {
 
--- app/src/main/java/com/polimi/proj/qdocs/support/StorageAdapter.java
+++ app/src/main/java/com/polimi/proj/qdocs/support/StorageAdapter.java
@@ -19,12 +19,31 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.StorageReference;
 import com.polimi.proj.qdocs.R;
+import com.polimi.proj.qdocs.model.MyDirectory;
+import com.polimi.proj.qdocs.model.MyFile;
+import com.polimi.proj.qdocs.model.StorageElement;
 
 import java.io.File;
 import java.io.FilenameFilter;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.List;
+
+/**
+ * Copyright 2018-2019 Lamparelli Andrea & Chittò Pietro
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 public abstract class StorageAdapter extends RecyclerView.Adapter<StorageAdapter.DataViewHolder>{
 
